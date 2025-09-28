805a091a199e70bec1b77b2eb10d55e08cd3ece4
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-09-28 12:40:29 +0100

    add namespace for cert-manager

--- helm.tf
+++ helm.tf
@@ -1,10 +1,21 @@
+resource "helm_release" "nginx" {
+  name       = "nginx-ingress"
+  repository = "https://helm.nginx.com/stable"
+  chart      = "nginx-ingress"
+
+  create_namespace = true
+  namespace = "nginx-ingress"
+}
+
 resource "helm_release" "cert_manager" {
   name             = "cert-manager"
   namespace        = kubernetes_namespace_v1.cert_manager.metadata[0].name
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
   version          = "v1.18.2"
-  create_namespace = false
+
+  create_namespace = true
+  namespace = "cert-manager"
 
   set = [
     { name = "installCRDs", value = "true" },
@@ -13,13 +24,4 @@ resource "helm_release" "cert_manager" {
   ]
 
   depends_on = [aws_eks_pod_identity_association.cert_manager]
-}
-
-resource "helm_release" "nginx" {
-  name       = "nginx-ingress"
-  repository = "https://helm.nginx.com/stable"
-  chart      = "nginx-ingress"
-
-  create_namespace = true
-  namespace = "nginx-ingress"
 }
