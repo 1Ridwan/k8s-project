4b0394220e63d9d99b79b21ac5cc1374cd148d30
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-10-25 22:30:26 +0100

    add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+# Delete helm releases
+helm uninstall argocd --namespace argocd
+helm uninstall cert-manager --namespace cert-manager
+helm uninstall external-dns --namespace external-dns
+helm uninstall nginx-ingress --namespace nginx-ingress
+
+# Delete ArgoCD related CRDs
+kubectl delete crd applications.argoproj.io \
+                   applicationsets.argoproj.io \
+                   appprojects.argoproj.io
+                
+# Delete Cert-Manager related CRDs
+kubectl delete crd certificaterequests.cert-manager.io \
+                   certificates.cert-manager.io \
+                   challenges.acme.cert-manager.io \
+                   clusterissuers.cert-manager.io \
+                   issuers.cert-manager.io \
+                   orders.acme.cert-manager.io
