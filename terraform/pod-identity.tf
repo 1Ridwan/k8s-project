c34f1f5a7d72b3e68ec78b3caf0a1f95ad7c365e
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-11-02 22:24:11 +0000

    remove commented code

--- terraform/pod-identity.tf
+++ terraform/pod-identity.tf
@@ -1,62 +1,3 @@
-# resource "helm_release" "ingress_nginx" {
-#   name             = "ingress-nginx"
-#   namespace        = "ingress-nginx"
-#   create_namespace = true
-#   repository       = "https://kubernetes.github.io/ingress-nginx"
-#   chart            = "ingress-nginx"
-
-#   version = "4.11.3"
-
-#   values = [file("helm-values/nginx.yaml")]
-
-#   cleanup_on_fail = true
-#   wait       = true
-#   timeout    = 900
-#   atomic     = false # so Helm won’t uninstall it if readiness takes a while
-# }
-
-
-
-# IAM role with the official LBC policy, via the pod-identity module
-
-# module "alb_controller_pod_identity" {
-#   source  = "terraform-aws-modules/eks-pod-identity/aws"
-#   version = "~> 1.9" # or newer
-
-#   name = "aws-load-balancer-controller"
-
-#   attach_aws_lb_controller_policy = true
-# }
-
-# resource "aws_eks_pod_identity_association" "alb" {
-#   cluster_name    = module.eks.cluster_name
-#   namespace       = "kube-system"
-#   service_account = "aws-load-balancer-controller"
-#   role_arn        = module.alb_controller_pod_identity.iam_role_arn
-# }
-
-## cert manager ##
-
-# resource "helm_release" "cert_manager" {
-#   name       = "cert-manager"
-#   repository = "https://charts.jetstack.io"
-#   chart      = "cert-manager"
-#   version    = "v1.18.2"
-
-#   create_namespace = true
-#   namespace        = "cert-manager"
-
-#   values = [
-#     "${file("helm-values/cert-manager.yaml")}",
-#   ]
-
-#   depends_on = [aws_eks_pod_identity_association.cert_manager]
-#   wait       = true
-#   timeout    = 900
-#   atomic     = false # so Helm won’t uninstall it if readiness takes a while
-
-# }
-
 module "cert_manager_pod_identity" {
   source = "terraform-aws-modules/eks-pod-identity/aws"
   name   = "cert-manager"
@@ -72,27 +13,6 @@ resource "aws_eks_pod_identity_association" "cert_manager" {
   service_account = "cert-manager"
   role_arn        = module.cert_manager_pod_identity.iam_role_arn
 }
-
-## external dns ##
-
-
-# resource "helm_release" "external_dns" {
-#   name       = "external-dns"
-#   repository = "https://charts.bitnami.com/bitnami"
-#   chart      = "external-dns"
-
-#   create_namespace = true
-#   namespace        = "external-dns"
-
-#   values = [
-#     "${file("helm-values/external-dns.yaml")}",
-#   ]
-
-#   depends_on = [aws_eks_pod_identity_association.external_dns]
-#   wait       = true
-#   timeout    = 900
-#   atomic     = false # so Helm won’t uninstall it if readiness takes a while
-# }
 
 
 module "external_dns_pod_identity" {
@@ -111,29 +31,6 @@ resource "aws_eks_pod_identity_association" "external_dns" {
   role_arn        = module.external_dns_pod_identity.iam_role_arn
 }
 
-## argocd
-
-# resource "helm_release" "argocd" {
-#   name       = "argocd"
-#   repository = "https://argoproj.github.io/argo-helm"
-#   chart      = "argo-cd"
-#   version    = "9.0.5"
-
-#   create_namespace = true
-#   namespace        = "argocd"
-
-#   force_update = true
-#   reuse_values = false
-
-#   values = [
-#     "${file("helm-values/argocd-values.yaml")}",
-#   ]
-
-#   wait       = true
-#   timeout    = 900
-#   atomic     = false # so Helm won’t uninstall it if readiness takes a while
-
-# }
 
 module "argocd_pod_identity" {
   source = "terraform-aws-modules/eks-pod-identity/aws"
@@ -152,27 +49,6 @@ resource "aws_eks_pod_identity_association" "argocd_repo" {
   role_arn        = module.argocd_pod_identity.iam_role_arn
 }
 
-## Prometheus
-
-# resource "helm_release" "prometheus" {
-#   name       = "prometheus"
-#   repository = "https://prometheus-community.github.io/helm-charts"
-#   chart      = "prometheus"
-#   version    = "27.42.0"
-
-#   create_namespace = true
-#   namespace        = "prometheus"
-
-#   values = [
-#     "${file("helm-values/prometheus.yaml")}",
-#   ]
-
-#   wait       = true
-#   timeout    = 900
-#   atomic     = false # so Helm won’t uninstall it if readiness takes a while
-
-# }
-
 module "amazon_managed_service_prometheus_pod_identity" {
   source = "terraform-aws-modules/eks-pod-identity/aws"
 
@@ -190,19 +66,3 @@ resource "aws_eks_pod_identity_association" "prometheus" {
   role_arn        = module.amazon_managed_service_prometheus_pod_identity.iam_role_arn
 }
 
-# Grafana
-
-# resource "helm_release" "grafana" {
-#   name       = "grafana"
-#   repository = "https://grafana.github.io/helm-charts"
-#   chart      = "grafana"
-
-#   namespace  = "monitoring"
-#   create_namespace = true
-
-#   values     = ["${file("helm-values/grafana.yaml")}"]
-
-#   wait       = true
-#   timeout    = 900
-#   atomic     = false # so Helm won’t uninstall it if readiness takes a while
-# }
