0ed6b30081563b956bba724d333f0c30c1f1d19c
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-11-08 11:24:14 +0000

    pin versions and remove providers for helm and kubernetes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,9 @@
 terraform {
-  required_version = ">= 1.12.0"
+  required_version = "~> 1.12.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = ">= 6.15.0, < 7.0" # my eks terraform module requires 6.15.0
     }
   }
 }
@@ -11,25 +12,25 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-provider "helm" {
-  kubernetes = {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
-  }
-}
+# provider "helm" {
+#   kubernetes = {
+#     host                   = module.eks.cluster_endpoint
+#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#     exec = {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+#       command     = "aws"
+#     }
+#   }
+# }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    command     = "aws"
-  }
-}
+# provider "kubernetes" {
+#   host                   = module.eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+#     command     = "aws"
+#   }
+# }
 
