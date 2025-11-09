793b454e19a5afd898db6fa09473752b3aadbeea
1Ridwan <97636059+1Ridwan@users.noreply.github.com>
2025-11-09 13:07:33 +0000

    change order of tasks

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+# Run this script after terraform resources are provisioned
+
+# Install helm resources
+helmfile apply
+
+# Configure ClusterIssuer
+kubectl apply -f prod-issuer.yaml
+
+# Create ArgoCD application
+kubectl apply -f argocd-app/application.yaml
